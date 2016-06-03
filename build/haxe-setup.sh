efae3aad64a897c8e0968a738a949845227581a5
SeiferTim <seifertim@gmail.com>
2016-06-03 14:28:13 -0500

    no message

---
+++ build/haxe-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+if [ ! -f ~/neko/neko-2.0.0-linux64.tar.gz ]; then
+  mkdir -p ~/neko
+  curl -s -L --retry 3 'http://nekovm.org/_media/neko-2.0.0-linux64.tar.gz' | tar -C ~/neko -x -z --strip-components=1 -f -
+fi
+if [ ! -f ~/haxe/haxe-3.2.0-linux64.tar.gz ]; then
+  mkdir -p ~/haxe
+  curl -s -L --retry 3 'http://haxe.org/website-content/downloads/3.2.0/downloads/haxe-3.2.0-linux64.tar.gz' | tar -C ~/haxe -x -z --strip-components=1 -f -
+fi
+mkdir -p ~/haxe/lib
+haxelib setup ~/haxe/lib
+haxelib install lime > /dev/null
+haxelib install openfl > /dev/null
+haxelib git flixel https://github.com/HaxeFlixel/flixel > /dev/null
+haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons > /dev/null
+haxelib git flixel-tools https://github.com/HaxeFlixel/flixel-tools > /dev/null
